9721ac6481414ca530b50765910926180d17d3bf
Ethan <etheaarygame@gmail.com>
2023-08-20 14:51:06 +1200

    Logout properly
    I forgot this part

--- panel/login/logout.php
+++ panel/login/logout.php
@@ -1,9 +1,11 @@
 <?php
     session_start();
+
+    // Reset the specific session variables
+    $_SESSION['flowsql__isloggedin'] = false;
     unset($_SESSION['flowsql__hostname']);
     unset($_SESSION['flowsql__username']);
     unset($_SESSION['flowsql__pass']);
-    session_abort();
     
     header("Location: ../../");
     exit;
